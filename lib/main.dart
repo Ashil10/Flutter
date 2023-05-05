e6259bf31804307f00abd33a7f449744f8cab19d
Ashil <ashilkp2003@gmail.com>
2023-05-05 16:04:42 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -11,14 +12,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme:ThemeData(primarySwatch: Colors.orange));
+        theme:ThemeData(primarySwatch: Colors.orange);
         home: Scaffold(
             appBar: AppBar(
       title: Text("Flutter"),
       centerTitle: true,
       foregroundColor: Colors.green,
       backgroundColor: CupertinoColors.label,
-    ))
+    )));
 
   }
 }
+*/
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:project1/day1/login_page.dart';
+void main()
+{
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home:LoginPage()
+    );
+  }
+}
+
