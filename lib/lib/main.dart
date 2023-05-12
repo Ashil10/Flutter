30135f068d0f21110edec0e58f4e45410c77b5ab
Ashil <ashilkp2003@gmail.com>
2023-05-12 15:31:12 +0530

    Add files via upload

---
+++ lib/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:project1/day5/AlertScreen.dart';
+import 'package:project1/day5/Screen1.dart';
+import 'package:project1/day6/SplashScreen.dart';
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+        home:AlertScreen()
+    );
+  }
+}
